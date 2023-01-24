b71b7f9e4a145af70e0d99eb16da91e0ba3ec504
Fabian Schwab <fabian.schwab@ibm.com>
2023-01-24 08:54:09 +0100

    fix

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # specify the repository URL
 repo_url="https://github.com/fabianschwab/openshift-tekton-pipeline/archive/refs/heads/main.zip"
 
-curl -LJO "$repo_url" && unzip -m main.zip -d ./tmp-installer/
+curl -LJO "$repo_url" && unzip -m openshift-tekton-pipeline-main.zip -d ./tmp-installer/
 
 mv -r ./tmp-installer/template ./tmp-installer/create-pipeline.sh . && rm -rf ./tmp-installer
 
